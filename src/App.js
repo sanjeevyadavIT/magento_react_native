012a159a58a367f645256b956f7a8d49be971c83
Sanjeev Yadav <sanjeevy133@protonmail.com>
2019-02-13 16:20:21 +0530

    REFACTORING: Created boilerplate folder

---
+++ src/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+
+class App extends Component{
+  render() {
+    return (
+      <View>
+        <Text>Magento 2 React Native</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
