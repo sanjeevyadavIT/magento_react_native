fb2c635a15aa151ca5b84615bc2dc633969f3ffe
Sanjeev Yadav <sanjeevy133@protonmail.com>
2020-06-01 00:53:03 +0530

    build: Configure eslint & prettier

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,7 @@
 module.exports = {
-  bracketSpacing: false,
-  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
   singleQuote: true,
   trailingComma: 'all',
+  arrowParens: 'avoid',
 };
